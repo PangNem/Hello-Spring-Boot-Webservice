e21e3cc1f36b56228caa834774f7a57d489f031e
GuGU <wnduddl3409@naver.com>
2019-12-18 20:11:09 +0900

    Create Mustache Index

---
+++ src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
@@ -0,0 +1,13 @@
+package com.gugu99.book.springboot.web.dto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
