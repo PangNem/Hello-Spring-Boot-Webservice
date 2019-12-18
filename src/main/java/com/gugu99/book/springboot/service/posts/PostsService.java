89f8d7c5d56c190a555a36597cc3393e90c75a7e
GuGU <wnduddl3409@naver.com>
2019-12-18 17:50:30 +0900

    ADD POST Routing

--- src/main/java/com/gugu99/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/gugu99/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.gugu99.book.springboot.service.posts;
 
+import com.gugu99.book.springboot.domain.posts.Posts;
 import com.gugu99.book.springboot.domain.posts.PostsRepository;
+import com.gugu99.book.springboot.web.dto.PostsResponseDto;
 import com.gugu99.book.springboot.web.dto.PostsSaveRequestDto;
+import com.gugu99.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
