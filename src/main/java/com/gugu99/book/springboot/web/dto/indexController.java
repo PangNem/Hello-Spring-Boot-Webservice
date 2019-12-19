b326af40693e92e69905390a27f4ec7e9093fc8d
GuGU <wnduddl3409@naver.com>
2019-12-19 17:52:23 +0900

    Update Posts Modify

--- src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
+++ src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
@@ -1,10 +1,13 @@
 package com.gugu99.book.springboot.web.dto;
 
 import com.gugu99.book.springboot.service.posts.PostsService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,4 +24,13 @@ public class indexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
 }
