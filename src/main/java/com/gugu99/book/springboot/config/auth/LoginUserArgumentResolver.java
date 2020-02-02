8dd435230c829a19866319c4bcddd276402716f5
PangNem <sprnd645@gmail.com>
2020-02-02 22:27:10 +0900

    [Update] update code

--- src/main/java/com/gugu99/book/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/gugu99/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -13,7 +13,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     private final HttpSession httpSession;
 
-
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
--- src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
+++ src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class indexController {
