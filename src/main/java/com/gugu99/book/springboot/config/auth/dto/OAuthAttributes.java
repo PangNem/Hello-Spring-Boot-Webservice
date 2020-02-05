2a48004076d8d9385165aa11fa0a8c8d0265be6f
PangNem <sprnd645@gmail.com>
2020-02-05 20:26:41 +0900

    [Update] ofNaver check

--- src/main/java/com/gugu99/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/gugu99/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -2,6 +2,7 @@ package com.gugu99.book.springboot.config.auth.dto;
 
 import com.gugu99.book.springboot.domain.user.Role;
 import com.gugu99.book.springboot.domain.user.User;
+import jdk.dynalink.NamedOperation;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -25,6 +26,10 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,6 +39,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
