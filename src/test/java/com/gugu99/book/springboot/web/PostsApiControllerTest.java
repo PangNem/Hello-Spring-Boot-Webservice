30c912160a270167c797c6b5fae407f155ff0848
PangNem <sprnd645@gmail.com>
2020-02-19 23:29:46 +0900

    [Update] import MockMvcBuilder

--- src/test/java/com/gugu99/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/gugu99/book/springboot/web/PostsApiControllerTest.java
@@ -18,9 +18,13 @@ import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
